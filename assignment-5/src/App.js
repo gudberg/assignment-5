a1eba34b4d5ad425d15bcee018662206c5d9dbd7
Gudberg Sumarlidason <gudberg_sumarlidason@Gudberg.local>
2018-03-18 13:04:36 +0000

    modal namecard carousel startingSpinner

--- assignment-5/src/App.js
+++ assignment-5/src/App.js
@@ -1,25 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Modal from './components/Modal/Modal';
 import NameCard from './components/NameCard/NameCard';
+import Carousel from './components/Carousel/Carousel';
+import Modal from './components/Modal/Modal';
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isModalOpen: false
+    };
+  }
+    
   render() {
+    const { isModalOpen } = this.state;
     return (
-      
-      <div className="container ">
-        <Modal color="small" > Modal texti </Modal>
-        <Modal color="medium" > Modal texti </Modal>
-        <Modal color="large" > Modal texti </Modal>
+      <div className="container">
+        
+        <button onClick={() => this.setState({ isModalOpen: true})}>Open Modal!</button>
+        <Modal 
+          isOpen={isModalOpen}
+          onClose={() => this.setState({ isModalOpen:false })}>
+          <Modal.Title>is title</Modal.Title>
+          <Modal.Body>is Body</Modal.Body>
+          <Modal.Footer>is Footer</Modal.Footer>
+        </Modal>
 
         <NameCard 
-          name="Gudberg Sumarlidason"
-          email="gudberg.sumarlidason@gmail.com"
-          telephone="773-8843"
-          imageUrl="http://www.visir.is/i/622E8CBFB96B099C70174D1235590FF956447390CFBC30D2FFED237508B7E9E4_713x0.jpg"
-          />
-      </div>
+          name="Nafn"
+          email="Email"
+          telephone="Simi"
+          imageUrl="https://www.wikihow.com/images/6/61/Draw-a-Cartoon-Man-Step-15.jpg"
+        />
+      
+        <Carousel 
+          images={[
+            'http://www.visir.is/i/622E8CBFB96B099C70174D1235590FF956447390CFBC30D2FFED237508B7E9E4_713x0.jpg',
+            'http://www.visir.is/i/92928A068E9F92A390DF808A575136185E131045ECBEDAEC582EFC04D8D6899D_713x0.jpg',
+            'https://scontent-lht6-1.xx.fbcdn.net/v/t31.0-8/17855110_10212686792633955_9078787707100677461_o.jpg?oh=d1fb663a2479044bf07bdb7b2acfc614&oe=5B433475',
+            'https://scontent-lht6-1.xx.fbcdn.net/v/t1.0-9/20294477_10213868745609799_2697690918225896195_n.jpg?oh=5927473e14feb07ca8c6eae45b4d1693&oe=5B3A9E21'
+          ]}
+          size="medium" />
+       </div>
       
     );
   }
--- assignment-5/src/components/Carousel/Carousel.js
+++ assignment-5/src/components/Carousel/Carousel.js
@@ -1,19 +1,77 @@
 import React from 'react';
 import Proptypes from 'prop-types';
-import styles from './Modal.css';
+import styles from './Carousel.css';
+import CarouselSlideItem from '../CarouselSlideItem/CarouselSlideItem';
 
-const Carousel = ({color, images}) => {
-    return(
-        <div style={{ backgroundImage: `url(${images})` }} className={styles[`avatar-${size}`]}></div>
-    );
+class Carousel extends React.Component  {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            currSlide : 0
+        }
+        this.forward= this.forward.bind(this);
+        this.backwards= this.forward.bind(this);
+    } 
+
+    //Movements to both sides 
+    backwards() {
+        let getCurrSlide = this.state.currSlide;        
+        if(getCurrSlide > 0) {
+            this.setState({
+                currSlide: getCurrSlide - 1
+            })
+        }
+        else {
+            this.setState({
+                currSlide: getCurrSlide -1 //In order to go to the last slide after having gone all the way down
+            })
+        } 
+    }
+
+    forward() {
+        let getCurrSlide = this.state.currSlide;
+        if(getCurrSlide< this.props.images.length -1) {
+            this.setState({
+                currSlide: getCurrSlide + 1
+            })
+        }
+        else {
+            this.setState({
+                currSlide: 0 //In order to go backwards if it has reached the end
+            })
+        } 
+    }
+ // Movement ends 
+
+    render() {
+        let imgArray = this.props.images;
+        let currSlide = this.state.currSlide;
+        var getThis = this;
+       let fetchImages = imgArray.map((item, slideNumber) => {
+            return(
+                <CarouselSlideItem getSize = {getThis.props.size} image ={item} show= {getThis.state.currSlide === slideNumber} />
+            );
+        }); 
+
+        return(
+            <div className={styles.slideShow}>
+                <a className={styles.next} onClick={this.backwards}>previous image</a>
+                <div className={styles.slider}>
+                     {fetchImages}
+                 </div>
+                <a className={styles.next} onClick={this.forward}>next image</a>
+            </div>
+        );
+    }
 };
+
 
 Carousel.propTypes = {
     size: Proptypes.oneOf([ 'small', 'medium', 'large' ]),
-    images: PropTypes.array.isRequired
+    images: Proptypes.array.isRequired
 };
 
-Carousel.defaultProps = {
-    size: 'medium'
-};
+
+
 export default Carousel;
