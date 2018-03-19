3fc9a7b1d1486998b8e0ec1b504dcd191ff12717
Alex Mar <alex16@ru.is>
2018-03-19 17:20:48 +0000

    Næstum búið, bara gá overflow scroll, þegar það eru fleiri en tíu töb

---
+++ assignment-5/src/components/Tabs/Tabs.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Tab from '../Tab/Tab';
+import styles from './Tabs.css';
+
+const Tabs = ({ children, currentSelectedTab, onSelect, theme }) => {
+    let indexOfKey = children.map(child => child.props.selectionKey);
+    const currentTabIndex = indexOfKey.indexOf(currentSelectedTab);
+    const currentTab = children[currentTabIndex];
+    const tabs = children.map((child, i) => {
+        let tabStyle = `${styles.tab}`;
+        if(i === currentTabIndex) {
+            tabStyle += ` ${styles[`selected-${theme}`]}`; 
+        } else {
+            tabStyle +=` ${styles[`not-${theme}`]}`
+        }
+        return (
+            <div key={i} onClick={() => onSelect(child.props.selectionKey)}
+                className={tabStyle}>
+                    {child.props.title}
+            </div>
+        );
+    });
+    return (
+        <div>   
+            <div className={`${styles[`${theme}`]}`}>
+                {tabs}
+            </div>
+            <div>
+                {currentTab}
+            </div>
+        </div>
+    );
+}
+
+Tabs.propTypes = {
+    theme: PropTypes.oneOf(['dark', 'light']),
+    layout: PropTypes.oneOf(['horizontal', 'vertical']),
+    onSelect: PropTypes.func.isRequired,
+    currentSelectedTab: PropTypes.number.isRequired,
+    children: (props, propName, componentName) => {
+        const prop = props[propName];
+        let error = null;
+        if(prop === undefined) {
+            error = new Error('Row should contain Col element');
+        }
+        React.Children.forEach(prop, (child) => {
+            if(child.type !== Tab) {
+                error = new Error(`${componentName} children should be of type Tab.`)
+            }
+        });
+        return error;
+    }
+}
+
+Tabs.defaultProps =  {
+    theme: 'light',
+    layout: 'horizontal'
+}
+
+export default Tabs;
